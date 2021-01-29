2cc445cb3f7e01e318ac81b6cee5361d624bea3e
Azlin Farid <2483116M@student.gla.ac.uk>
2021-01-29 22:23:55 +0000

    setup Django admin site

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+#to include the models we previously created(Category & Page)
+from django.contrib import admin
+from rango.models import Category,Page
+
+admin.site.register(Category)
+admin.site.register(Page)
--- rango/models.py
+++ rango/models.py
@@ -4,14 +4,20 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
 
+    #typo within admin interface can be fixed using code below.On the page, it was Categorys instead of Categories
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     def __str__(self):
         return self.name
 
 class Page(models.Model):
+    #CASCADE instructs Django to delete the pages associated with the category when the category is deleted
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
 
+    #Django will display the string representation of the object, derived from __str__(). adding __str()__ will make debugging easy too
     def __str__(self):
         return self.title
