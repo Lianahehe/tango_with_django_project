b201d734fd15f706ac17f27056c6ef6ee64bf467
Azlin Farid <2483116M@student.gla.ac.uk>
2021-02-04 14:52:58 +0000

    model setupand customised admin interface

--- rango/models.py
+++ rango/models.py
@@ -4,6 +4,10 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
 
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+
+
     #typo within admin interface can be fixed using code below.On the page, it was Categorys instead of Categories
     class Meta:
         verbose_name_plural = 'Categories'
